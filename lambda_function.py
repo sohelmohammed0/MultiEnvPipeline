a434d5c642e1cd2513383f5270c962484a2285c1
sohelmohammed0 <sohelmohammed88004329@gmail.com>
2026-03-05 18:20:34 +0530

    prod-1

--- lambda_function.py
+++ lambda_function.py
@@ -6,9 +6,9 @@ def handler(event, context):
         },
         "body": """
         <html>
-            <body style="background-color:blue;">
+            <body style="background-color:green;">
                 <h1 style="color:white;text-align:center;margin-top:20%;">
-                    BLUE test LAMBDA
+                    GREEN prod LAMBDA
                 </h1>
             </body>
         </html>
